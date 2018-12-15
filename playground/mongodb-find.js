367a27ec6aaf9344604cb2d0bf14d71ecbeeb913
Kris Montgomery <kmontgomery91@gmail.com>
2018-12-15 14:33:40 -0600

    JS file with the ability in getting stored objects in the database

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -8,12 +8,38 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     }
     console.log('Connected to MongoDB Server');
 
-    db.collection('Todos').find().toArray().then((docs) => {
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('5c1549d7b2dbd402d1a67cbb')
+    // }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
 
-    }, (err) => {
-        console.log('Unable to fetch todos', err);
-    });
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
  
 
-    db.close();
+
+    // getting the number of named users specified in the DB
+//  db.collection('Users').find({name: 'Kris Montgomery'}).count().then((count) => {
+//         console.log(`Todos count: ${count}`);
+//     }, (err) => {
+//         console.log('Unable to fetch todos', err);
+//     });
+
+
+
+// Getting the user objects and meta data displayed to us with specific named person
+db.collection('Users').find({name: 'Kris Montgomery'}).toArray().then((docs) => {
+    console.log(JSON.stringify(docs, undefined, 2));
+});
+
+
+
+    // db.close();
 });
