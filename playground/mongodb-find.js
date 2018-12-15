7b22e217070f5acefe8840449efcc9cede837222
Kris Montgomery <kmontgomery91@gmail.com>
2018-12-15 13:24:14 -0600

    second commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,19 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB Server');
+
+    db.collection('Todos').find().toArray().then((docs) => {
+
+    }, (err) => {
+        console.log('Unable to fetch todos', err);
+    });
+ 
+
+    db.close();
+});
